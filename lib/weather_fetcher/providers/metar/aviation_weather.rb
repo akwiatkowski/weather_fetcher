bcb1cac26449159448e1070b8d32170886f5ad0f
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-01-05 12:17:34 +0100

    other metar providers (not ready)

---
+++ lib/weather_fetcher/providers/metar/aviation_weather.rb
@@ -0,0 +1,27 @@
+#encoding: utf-8
+
+module WeatherFetcher
+  class Provider::AviationWeather < Metar
+
+    def url_for_metar(metar_city)
+      u = "http://aviationweather.gov/adds/metars/index.php?submit=1&station_ids=#{metar_city.upcase}"
+      return u
+    end
+
+    def process(string)
+      reg = /\">([^<]*)<\/FONT>/
+      data = string.scan(reg).first
+      if not data.nil? and not data.first.nil?
+        string = data.first
+        string.gsub!(/\n/, ' ')
+        string.gsub!(/\t/, ' ')
+        string.gsub!(/\s{2,}/, ' ')
+        @weathers << string.strip
+      end
+    end
+
+  end
+
+end
+
+
---
+++ lib/weather_fetcher/providers/metar/wunderground.rb
@@ -0,0 +1,24 @@
+#encoding: utf-8
+
+module WeatherFetcher
+  class Provider::Wunderground < Metar
+
+    def url_for_metar(metar_city)
+      u = "http://www.wunderground.com/Aviation/index.html?query=#{metar_city.upcase}"
+      return u
+    end
+
+    def process(string)
+      reg = /<div class=\"textReport\">\s*METAR\s*([^<]*)<\/div>/
+      string = string.scan(reg).first.first
+      string.gsub!(/\n/, ' ')
+      string.gsub!(/\t/, ' ')
+      string.gsub!(/\s{2,}/, ' ')
+      @weathers << string.strip
+    end
+
+  end
+
+end
+
+
