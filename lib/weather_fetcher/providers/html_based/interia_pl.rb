93092505bbac2c8ca696126b35b945fe8fbb3fb2
Aleksander Kwiatkowski <bobikx@poczta.fm>
2013-12-24 14:34:20 +0100

    new Interia.pl provider

--- lib/weather_fetcher/providers/html_based/interia_pl.rb
+++ lib/weather_fetcher/providers/html_based/interia_pl.rb
@@ -26,53 +26,46 @@ module WeatherFetcher
 
         weather_list = d.css("li.weather-entry")
         weather_list.each do |w|
-          h = Hash.new
-
           hour = w.css(".entry-hour > .hour").children.first.to_s.to_i
           min = w.css(".entry-hour > .minute").children.first.to_s.to_i
           time_from = t + hour * 3600 + min * 60
 
-          puts time_from
+          temp = w.css(".forecast-top > .forecast-temp").children.first.to_s.to_f
+          feel_temp = w.css(".forecast-top > .forecast-feeltemp").children.first.to_s.gsub(/Odczuwalna/, "").to_f
+          wind = w.css(".wind-speed > .speed-value").children.first.to_s.to_f
+          rain = w.css("span.entry-precipitation-value.rain").children.first.to_s.gsub(/,/, ".").to_f
+          snow = w.css("span.entry-precipitation-value.snow").children.first
+          if snow
+            snow = snow.to_s.gsub(/,/, ".").to_f
+          else
+            snow = nil
+          end
+          cloud_cover = w.css("span.entry-precipitation-value.cloud-cover").children.first.to_s.to_f
+          humidity = w.css(".entry-humidity").children.first.to_s.to_f
+          # puts humidity.inspect
+
+          h = {
+            :time_created => Time.now,
+            :time_from => time_from,
+            :time_to => time_from + 3600,
+            :temperature => temp,
+            :feel_temperature => feel_temp,
+            #  :pressure => pressures[0][0].to_f,
+            :wind_kmh => wind,
+            :wind => wind / 3.6,
+            #  :snow => nil, #snows[0][0].to_f,
+            :rain => rain,
+            :cloud_cover => cloud_cover,
+            :humidity => humidity,
+            :provider => self.class.provider_name
+          }
+
+          data << h
         end
 
       end
 
-      #days_count = 3
-      #(0...days_count).each do |di|
-      #  day_t = unix_time_today + 24*3600*di
-      #  day_s = day_t.strftime("%Y-%m-%d")
-      #
-      #  css_selector = ".weather-forecast-day.#{day_s} > li.weather-entry"
-      #  puts css_selector
-      #
-      #  weather_list = b.css(css_selector)
-      #  weather_list.each do |w|
-      #    h = Hash.new
-      #
-      #    hour = w.css(".entry-hour > .hour").children.first.to_s.to_i
-      #    min = w.css(".entry-hour > .minute").children.first.to_s.to_i
-      #    time_from = day_t + hour * 3600 + min * 60
-      #
-      #    puts time_from
-      #
-      #  end
-      #end
-
-
-      #h = {
-      #  :time_created => Time.now,
-      #  :time_from => time_from,
-      #  :time_to => time_from + 3600,
-      #  :temperature => temperatures[1 + i][0].to_f,
-      #  :pressure => pressures[0][0].to_f,
-      #  :wind_kmh => winds[0][0].to_f,
-      #  :wind => winds[0][0].to_f / 3.6,
-      #  :snow => nil, #snows[0][0].to_f,
-      #  :rain => rains[0][0].gsub(/,/, '.').to_f,
-      #  :provider => self.class.provider_name
-      #}
-
-      return
+      return data
     end
   end
 end
